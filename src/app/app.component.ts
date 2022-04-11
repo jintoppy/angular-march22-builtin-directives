cb3561850f6a59d3980ffe50d3c31911ed6dd7bd
Jinto Jose <jintoppy@gmail.com>
2022-04-11 14:28:05 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Angular Training';
+  name= '';
+  email = '';
+
+  users = [
+    {name: 'Vijay', age: 10},
+    {name: 'Ajith', age: 20}
+  ];
+
+
+  setName(e: KeyboardEvent){
+    const el = (e.target) as HTMLInputElement;
+    this.name = el.value;
+  }
+
+  setEmail(e: KeyboardEvent){
+    const el = (e.target) as HTMLInputElement;
+    this.email = el.value;
+  }
+
+  onClick(e: MouseEvent, myStr: string){
+    this.title = 'My Angular Training Completed';
+  }
+  
 }
+
+
+//tagName
+//class <-> template 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { MyButtonComponent } from './components/my-button/my-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent,
+    MyButtonComponent
   ],
   imports: [
     BrowserModule
