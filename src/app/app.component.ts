1d4cae03b33e1bb098476a9060612a0945b729d4
Jinto Jose <jintoppy@gmail.com>
2022-04-12 15:24:08 +0200

    error message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ type User = {
   age: number;
 }
 
+type ValidationMessage = {
+  isInvalid: boolean;
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +18,21 @@ type User = {
 export class AppComponent {
   selectedUser?: User;
   myVal = 'hello';
+  isNewNameValid = false;
+  isNewAgeValid = false;
+  isAddClicked = false;
+  messages = {
+    name: {
+      success: 'Name is valid',
+      error: 'Number not allowed in name'
+    },
+    age: {
+      success: 'Age is valid',
+      error: `Age can't be less than 1`
+    }
+  }
+
+  // nameErrorMsg = 'Number not allowed in name';
 
   newName = '';
   newAge = '';
@@ -22,7 +42,35 @@ export class AppComponent {
     {name: 'Ajith', age: 20}
   ];
 
-  onAdd(){
+  isNameInvalid(){
+    return /\d/.test(this.newName);
+  }
+
+  isAgeInvalid(){
+    const newAge = parseInt(this.newAge);
+    return !newAge || newAge < 1;
+  }
+
+  getValidationMessages(){
+    const nameMessage = {
+      isInvalid: this.isNameInvalid(),
+      message:   this.isNameInvalid()? this.messages.name.error : this.messages.name.success
+    };
+    const ageMessage = {
+      isInvalid: this.isAgeInvalid(),
+      message:   this.isAgeInvalid()? this.messages.age.error : this.messages.age.success
+    }
+    return [nameMessage, ageMessage];
+  }
+
+  currentMessages: ValidationMessage[] = [];
+
+  onAdd(){    
+    this.isAddClicked = true;    
+    this.currentMessages = this.getValidationMessages();
+    if(this.isNameInvalid() || this.isAgeInvalid()){
+      return;
+    }
     const newUser = {
       name: this.newName,
       age: parseInt(this.newAge)
